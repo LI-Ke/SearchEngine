204a34d12cb6f128292a2500f8c00818a115d5c7
Ke Li <li_ke@yahoo.com>
2016-11-26 17:35:59 +0100

    Create RandomWalk.py

---
+++ TP/liens/RandomWalk.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 26 17:22:41 2016
+
+@author: 3402901
+"""
+
+class RandomWalk(object):
+    '''
+    classRandomWalk
+    '''
+    def __init__(self):
+        '''
+        Constructor
+        '''
+         
+    def randomWalk(self, successuers, predecesseurs): 
+        raise NotImplementedError
