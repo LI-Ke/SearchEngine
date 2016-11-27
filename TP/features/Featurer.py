8d89ba32c95cf2bfcbbbe3f9866659dbb909f483
Ke Li <li_ke@yahoo.com>
2016-11-27 23:34:14 +0100

    Create Featurer.py

---
+++ TP/features/Featurer.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 27 23:12:05 2016
+
+@author: like
+"""
+
+class Featurer(object):
+    '''
+    classFeaturer
+    '''
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        
+    def getFeatures(self, idDoc, query): 
+        raise NotImplementedError
+        
+    
