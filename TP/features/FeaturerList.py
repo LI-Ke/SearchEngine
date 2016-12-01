b51a95b63ff80bff9794cb883fbf6467f9173cbf
Ke Li <li_ke@yahoo.com>
2016-12-01 12:06:44 +0100

    Update FeaturerList.py

--- TP/features/FeaturerList.py
+++ TP/features/FeaturerList.py
@@ -5,6 +5,18 @@ Created on Mon Nov 28 17:25:18 2016
 @author: like
 """
 from Featurer import Featurer
+from FeaturesLengthDoc import FeaturesLengthDoc
+from FeaturesSumIdfsQuery import FeaturesSumIdfsQuery
+from FeaturerModel import FeaturerModel
+import sys
+sys.path.insert(0, r'/home/like/M2/RI/TP/modeles')
+from WeighterTf1 import WeighterTf1
+sys.path.insert(0, r'/home/like/M2/RI/TP/evaluation')
+from QueryParserCACM import QueryParserCACM
+import numpy as np
+sys.path.insert(0, r'/home/like/M2/RI/TP')
+from index import Index
+from TextRepresenter import PorterStemmer
 
 class FeaturerList(Featurer):
     '''
@@ -20,3 +32,23 @@ class FeaturerList(Featurer):
             
         return featuresList
         
+if __name__ == '__main__':
+    textRepresenter = PorterStemmer()
+    source = "../cacm/cacm.txt"
+    index = Index("../document",source,textRepresenter)
+    index.indexation()
+    q = QueryParserCACM()
+    q.initFile("../cacm/cacm.qry","../cacm/cacm.rel")
+    query = q.nextQuery()
+    representationQuery = textRepresenter.getTextRepresentation(query.text)
+    weighter = WeighterTf1(index)
+    liste = []
+    f = FeaturesLengthDoc(weighter)
+    liste.append(f)
+    f = FeaturesSumIdfsQuery(weighter)
+    liste.append(f)
+    f = FeaturerModel(1, index, weighter)
+    liste.append(f)
+    featureList = FeaturerList(liste)
+    features = featureList.getFeatures('1', representationQuery)
+    print features
